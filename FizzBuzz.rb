254ec263ec74a2830d81b449d773b1acef6a38a4
JoeMugwaneza <muganezajoseph@gmail.com>
2017-03-08 10:42:29 +0200

    refactored my FizzBuzz test

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -2,15 +2,10 @@ require 'rspec'
 
 class Fizzbuzz
   def output(number)
-    if (number %3 == 0 && number %5 == 0)
-      return "FizzBuzz"
-     elsif number %3 == 0
-      return "Fizz"
-
-    elsif number %5 == 0
-      return "Buzz"
-    else        
-      return number
+    return "FizzBuzz" if (number % 15 == 0)
+    return "Fizz" if number %3 == 0
+    return "Buzz" if number %5 == 0      
+    return number
     end
   end
 
